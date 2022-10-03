51eb6d6c3b95f4939a4221cd0c882917a98045a3
Clucas0311 <clucas0311@gmail.com>
2022-10-03 15:11:52 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>My App</p>
+    </div>
+  );
+};
+
+export default App;
